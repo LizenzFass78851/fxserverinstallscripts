26128b1bc1e776b46286133d732f76a8b4fe5f80
LizenzFass78851 <82592556+LizenzFass78851@users.noreply.github.com>
2023-01-25 16:19:47 +0100

    porter change for the db backup function

---
+++ backup-solution/fivem-db-backup.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+for (( ; ; ))
+do
+   borg create -v -s -p -C lz4 /$(cd ~ && cd .. && ls | grep "$(whoami)")/server/backup/fivem-db::phpmyadmin-$(date '+%Y-%m-%d-%H:%M:%S') /$(cd ~ && cd .. && ls | grep "$(whoami)")/server/docker/phpmyadmin/db
+   borg prune -v --list --keep-within=1d --keep-daily=7 --keep-weekly=4 --keep-monthly=12 /$(cd ~ && cd .. && ls | grep "$(whoami)")/server/backup/fivem-db
+   sleep 24h
+done
