7f20b25fdd709b9d8aac172144b1d90fc5b9ec84
LizenzFass78851 <82592556+LizenzFass78851@users.noreply.github.com>
2023-05-06 09:28:51 +0200

    Update install-docker_with_docker-compose_ubuntu-server.sh
    fix recent docker installation errors on ubuntu and linux mint

--- scripts/install-phpmyadmin/docker-compose/install-docker_with_docker-compose_ubuntu-server.sh
+++ scripts/install-phpmyadmin/docker-compose/install-docker_with_docker-compose_ubuntu-server.sh
@@ -9,17 +9,17 @@ apt update
 apt install -y \
     ca-certificates \
     curl \
-    gnupg \
-    lsb-release
+    gnupg
 
-mkdir -p /etc/apt/keyrings
-
+install -m 0755 -d /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+chmod a+r /etc/apt/keyrings/docker.gpg
 
 echo \
-  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
-  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+  "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  "$(. /etc/os-release && echo "$UBUNTU_CODENAME")" stable" | \
+  tee /etc/apt/sources.list.d/docker.list > /dev/null
 
 apt update
 
-apt install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin docker-compose
+apt install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin docker-compose
