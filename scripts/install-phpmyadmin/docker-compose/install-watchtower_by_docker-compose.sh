83af31469eda6221aed24db868094c794bca1df5
LizenzFass78851 <82592556+LizenzFass78851@users.noreply.github.com>
2023-05-30 19:23:06 +0200

    Create install-watchtower_by_docker-compose.sh

---
+++ scripts/install-phpmyadmin/docker-compose/install-watchtower_by_docker-compose.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# script
+cd ~
+mkdir server
+cd server
+
+
+mkdir docker
+cd docker
+
+mkdir watchtower
+cd watchtower
+
+wget https://github.com/LizenzFass78851/fxserverinstallscripts/raw/main/files/watchtower/docker-compose.yml
+
+
+
+docker-compose up -d
