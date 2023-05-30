9ecde88856fedbc982642cea1b4f6cb3b4726311
LizenzFass78851 <82592556+LizenzFass78851@users.noreply.github.com>
2023-05-30 19:29:39 +0200

    Update install-autom-updater.sh

--- scripts/after-install-fivem-and-phpmyadmin/install-autom-updater.sh
+++ scripts/after-install-fivem-and-phpmyadmin/install-autom-updater.sh
@@ -37,37 +37,4 @@ systemctl start fivemupdater.service
 echo "systemctl status fivemupdater.service" can be used to query the status of the service
 
 
-# script 2
-cd ~
-cd server
 
-
-cd docker
-
-
-mkdir _autom-updater
-cd _autom-updater
-
-
-wget https://github.com/LizenzFass78851/fxserverinstallscripts/raw/multiuser/_autom-updater/docker-image-updater.sh
-chmod +x docker-image-updater.sh
-
-
-echo [Unit] >/etc/systemd/system/dockerupdater.service
-echo # Abschnitt wird im Artikel systemd/Units beschrieben >>/etc/systemd/system/dockerupdater.service
-echo Description=start Docker Image Updater >>/etc/systemd/system/dockerupdater.service
-echo  >>/etc/systemd/system/dockerupdater.service
-echo [Service] >>/etc/systemd/system/dockerupdater.service
-echo Type=simple >>/etc/systemd/system/dockerupdater.service
-echo ExecStart=/$(cd ~ && cd .. && ls | grep "$(whoami)")/server/docker/_autom-updater/docker-image-updater.sh >>/etc/systemd/system/dockerupdater.service
-echo User=$(whoami) >>/etc/systemd/system/dockerupdater.service
-echo Group=$(whoami) >>/etc/systemd/system/dockerupdater.service
-echo WorkingDirectory=/$(cd ~ && cd .. && ls | grep "$(whoami)")/server/docker/_autom-updater >>/etc/systemd/system/dockerupdater.service
-echo  >>/etc/systemd/system/dockerupdater.service
-echo [Install] >>/etc/systemd/system/dockerupdater.service
-echo # Abschnitt wird im Artikel systemd/Units beschrieben >>/etc/systemd/system/dockerupdater.service
-echo WantedBy=multi-user.target >>/etc/systemd/system/dockerupdater.service
-
-systemctl enable dockerupdater.service
-systemctl start dockerupdater.service
-echo "systemctl status dockerupdater.service" can be used to query the status of the service
