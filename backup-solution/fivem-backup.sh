e1f908b197f68abb49723b99d55a8e57de5da665
LizenzFass78851 <82592556+LizenzFass78851@users.noreply.github.com>
2023-06-02 18:04:16 +0200

    Update fivem-backup.sh

--- backup-solution/fivem-backup.sh
+++ backup-solution/fivem-backup.sh
@@ -2,6 +2,6 @@
 for (( ; ; ))
 do
    borg create -v -s -p -C lz4 /$(cd ~ && cd .. && ls | grep "$(whoami)")/server/backup/fivem::fivemserver-$(date '+%Y-%m-%d-%H:%M:%S') /txData
-   borg prune -v --list --keep-within=1d --keep-daily=7 --keep-weekly=4 --keep-monthly=12 /$(cd ~ && cd .. && ls | grep "$(whoami)")/server/backup/fivem 
-   sleep 24h
+   borg prune -v --list --keep-within=4d --keep-daily=7 --keep-weekly=4 --keep-monthly=12 /$(cd ~ && cd .. && ls | grep "$(whoami)")/server/backup/fivem 
+   sleep 6h
 done
