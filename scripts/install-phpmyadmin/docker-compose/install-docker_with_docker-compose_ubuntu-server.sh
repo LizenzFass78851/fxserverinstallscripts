4f79f9c82e8f80844735534f83ec254804dc0d7f
LizenzFass78851 <82592556+LizenzFass78851@users.noreply.github.com>
2023-10-31 08:23:12 +0100

    Update install-docker_with_docker-compose_ubuntu-server.sh

--- scripts/install-phpmyadmin/docker-compose/install-docker_with_docker-compose_ubuntu-server.sh
+++ scripts/install-phpmyadmin/docker-compose/install-docker_with_docker-compose_ubuntu-server.sh
@@ -22,4 +22,7 @@ echo \
 
 apt update
 
-apt install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin docker-compose
+apt install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+curl -SL https://github.com/docker/compose/releases/download/v2.23.0/docker-compose-linux-x86_64 -o /usr/local/bin/docker-compose
+ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
