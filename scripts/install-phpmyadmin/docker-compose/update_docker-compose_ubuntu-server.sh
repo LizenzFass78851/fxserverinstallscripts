5c91408052f62c7d2a7ca97ccc60e911af0ea78a
LizenzFass78851 <82592556+LizenzFass78851@users.noreply.github.com>
2023-11-02 16:41:58 +0100

    Add files via upload

---
+++ scripts/install-phpmyadmin/docker-compose/update_docker-compose_ubuntu-server.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+
+# script
+apt purge docker-compose -y
+apt autoremove -y
+
+rm /usr/local/bin/docker-compose 
+
+curl -SL $(curl -L -s https://api.github.com/repos/docker/compose/releases/latest | grep -o -E "https://(.*)docker-compose-linux-$(uname -m)") -o /usr/local/bin/docker-compose
+ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
