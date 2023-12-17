dbe18300fd4ae1fdd1aa6805ec0ba1e5259c40fb
LizenzFass78851 <82592556+LizenzFass78851@users.noreply.github.com>
2023-12-17 10:58:47 +0100

    Update fivem-updater.sh

--- _autom-updater/fivem-updater.sh
+++ _autom-updater/fivem-updater.sh
@@ -4,30 +4,36 @@ do
    SRV_ADR="https://runtime.fivem.net/artifacts/fivem/build_proot_linux/master/"
    
    # script
+   echo change dir
    cd ~
    cd server
    cd fivem
    
    
    #DL_URL=${SRV_ADR}"$(wget -q -O - ${SRV_ADR} | head -n 31 | tail -n 1 | cut -d '"' -f 4 | cut -c 2-)"
+   #echo downloading ${DL_URL}
    #wget ${DL_URL}
    	if [ ! -f ./fx.tar.xz ]; then
    		DL_URL=${SRV_ADR}"$(wget -q -O - ${SRV_ADR} | grep -B 1 'LATEST RECOMMENDED' | tail -n -2 | head -n -1 | cut -d '"' -f 2 | cut -c 2-)";
-   		wget ${DL_URL}
+   		echo downloading ${DL_URL}
+                wget ${DL_URL}
    		if [ ! -f ./fx.tar.xz ]; then
    			exit 1;
    		fi
    	fi
    
-   
+   echo stop fivem service and remove old program files
    systemctl stop fivemserver.service
    del -r alpine
    del run.sh
    
+   echo extract downloaded file
    tar -xvf fx.tar.xz
    rm fx.tar.xz
    
-   
+   echo start fivem service
    systemctl start fivemserver.service
+
+   echo Waiting for next time to create Backup
    sleep 48h
 done
