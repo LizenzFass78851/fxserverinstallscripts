cf59687e2a82f708379e6a79a207a144171b64dc
LizenzFass78851 <82592556+LizenzFass78851@users.noreply.github.com>
2024-01-08 16:07:27 +0100

    Update install-docker_with_docker-compose_ubuntu-server.sh

--- scripts/install-phpmyadmin/docker-compose/install-docker_with_docker-compose_ubuntu-server.sh
+++ scripts/install-phpmyadmin/docker-compose/install-docker_with_docker-compose_ubuntu-server.sh
@@ -2,27 +2,7 @@
 
 
 # script
-apt remove -y docker docker-engine docker.io containerd runc
-
-apt update
-
-apt install -y \
-    ca-certificates \
-    curl \
-    gnupg
-
-install -m 0755 -d /etc/apt/keyrings
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
-chmod a+r /etc/apt/keyrings/docker.gpg
-
-echo \
-  "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
-  "$(. /etc/os-release && echo "$UBUNTU_CODENAME")" stable" | \
-  tee /etc/apt/sources.list.d/docker.list > /dev/null
-
-apt update
-
-apt install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+curl -sSL https://get.docker.com | bash
 
 curl -SL $(curl -L -s https://api.github.com/repos/docker/compose/releases/latest | grep -o -E "https://(.*)docker-compose-linux-$(uname -m)") -o /usr/local/bin/docker-compose
 ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
