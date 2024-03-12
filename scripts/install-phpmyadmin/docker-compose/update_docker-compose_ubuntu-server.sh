8b89a9ef3e8e6eea47bdec2c0b9e767dea75dc28
LizenzFass78851 <82592556+LizenzFass78851@users.noreply.github.com>
2024-03-12 16:39:57 +0100

    Update update_docker-compose_ubuntu-server.sh

--- scripts/install-phpmyadmin/docker-compose/update_docker-compose_ubuntu-server.sh
+++ scripts/install-phpmyadmin/docker-compose/update_docker-compose_ubuntu-server.sh
@@ -8,6 +8,5 @@ apt autoremove -y
 rm /usr/local/bin/docker-compose
 rm /usr/bin/docker-compose
 
-curl -SL $(curl -L -s https://api.github.com/repos/docker/compose/releases/latest | grep -o -E "https://(.*)docker-compose-linux-$(uname -m)") -o /usr/local/bin/docker-compose
-ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
-chmod +x /usr/local/bin/docker-compose
+echo -e '#!/bin/sh\nexec docker compose "$@"' | tee /usr/bin/docker-compose && \
+  chmod +x /usr/bin/docker-compose
