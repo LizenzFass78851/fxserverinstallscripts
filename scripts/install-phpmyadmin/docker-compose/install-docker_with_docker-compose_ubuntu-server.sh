44cc7d5675659bf09e60f2f4ceeb697eba2dd5e0
LizenzFass78851 <82592556+LizenzFass78851@users.noreply.github.com>
2024-03-12 18:08:57 +0100

    Update install-docker_with_docker-compose_ubuntu-server.sh

--- scripts/install-phpmyadmin/docker-compose/install-docker_with_docker-compose_ubuntu-server.sh
+++ scripts/install-phpmyadmin/docker-compose/install-docker_with_docker-compose_ubuntu-server.sh
@@ -4,5 +4,6 @@
 # script
 curl -sSL https://get.docker.com | bash
 
-echo -e '#!/bin/sh\nexec docker compose "$@"' | tee /usr/bin/docker-compose && \
-  chmod +x /usr/bin/docker-compose
+echo -e '#!/bin/sh\nexec docker compose "$@"' | tee /usr/local/bin/docker-compose && \
+  ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose && \
+  chmod +x /usr/local/bin/docker-compose
