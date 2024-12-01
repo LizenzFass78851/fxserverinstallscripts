e634a796b484d0400003dd7e43ccade7c1e39371
LizenzFass78851 <82592556+LizenzFass78851@users.noreply.github.com>
2024-12-01 10:16:44 +0100

    Update 0-aio_installscript-lxc.sh

--- 0-aio_installscript-lxc.sh
+++ 0-aio_installscript-lxc.sh
@@ -7,7 +7,6 @@ GITFOLDERNAME=fxserverinstallscripts-$RANDOMFOLDER
 GITFOLDERBRANCH=main
 GITREPOLINK=https://github.com/LizenzFass78851/fxserverinstallscripts
 
-
 apt update && \
   apt install -yy git curl wget
 
@@ -19,11 +18,13 @@ cd $GITFOLDERNAME
 
 SCRIPTS=$(find  $PWD/ | grep ".sh$" | grep "/scripts/" | grep -v "/100-" | grep -v "/docker-compose" | sort)
 
+LOGFILE='~/$(date '+%Y-%m-%d-%H:%M:%S')-aio_installscript-vm.log'
+
 for SCRIPT in ${SCRIPTS}; do
-        echo =============================================================
-	echo RUN $SCRIPT
-        chmod +x $SCRIPT
-	$SCRIPT
+    echo ============================================================= | tee -a $LOGFILE
+    echo RUN $SCRIPT | tee -a $LOGFILE
+    chmod +x $SCRIPT
+    $SCRIPT 2>&1 | tee -a $LOGFILE
 done
 
 rm -rf /tmp/$GITFOLDERNAME
