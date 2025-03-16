00b1734bf348ff003755be5223dd1fae4f5ffb0f
LizenzFass78851 <82592556+LizenzFass78851@users.noreply.github.com>
2025-03-16 13:18:46 +0000

    refactor(fivemupdater): optimization of the script

--- _files/autom-updater/fivem-updater.sh
+++ _files/autom-updater/fivem-updater.sh
@@ -2,13 +2,15 @@
 
 set -e # Exit the script on error
 
+SRV_ADR="https://runtime.fivem.net/artifacts/fivem/build_proot_linux/master/"
+SERVER_DIR=~/server/fivem
+UPDATE_INTERVAL=48h
+
 while true
 do
-   SRV_ADR="https://runtime.fivem.net/artifacts/fivem/build_proot_linux/master/"
-   
    # script
-   echo change dir
-   cd ~/server/fivem
+   echo "Changing directory to ${SERVER_DIR}"
+   cd ${SERVER_DIR}
    
    # for recommended versions
    DL_URL=${SRV_ADR}"$(wget -q -O - ${SRV_ADR} | grep -B 1 'LATEST RECOMMENDED' | tail -n -2 | head -n -1 | cut -d '"' -f 2 | cut -c 2-)"
@@ -18,33 +20,34 @@ do
    #DL_URL=https://runtime.fivem.net/artifacts/fivem/build_proot_linux/master/9956-41b2e627e3b80ddbba4d63cb74968ac3d5926eb6/fx.tar.xz
 
    if [ -f ./fx.tar.xz ]; then
-        echo remove leftover files
+        echo "Removing leftover files"
         rm ./fx.tar.xz*
    fi
 
-   echo downloading ${DL_URL}
+   echo "Downloading ${DL_URL}"
    wget ${DL_URL}
-   	if [ ! -f ./fx.tar.xz ]; then
-                echo waiting 5 seconds
-                sleep 5s
-   		echo downloading ${DL_URL} again
-                wget ${DL_URL}
-   		if [ ! -f ./fx.tar.xz ]; then
-   			exit 1;
-   		fi
-   	fi
+   if [ ! -f ./fx.tar.xz ]; then
+        echo "Waiting 5 seconds before retrying download"
+        sleep 5s
+        echo "Downloading ${DL_URL} again"
+        wget ${DL_URL}
+        if [ ! -f ./fx.tar.xz ]; then
+            echo "Failed to download ${DL_URL}, exiting"
+            exit 1
+        fi
+   fi
    
-   echo stop fivem service and remove old program files
+   echo "Stopping FiveM service and removing old program files"
    systemctl stop fivemserver.service
    rm -rf alpine run.sh
    
-   echo extract downloaded file
+   echo "Extracting downloaded file"
    tar -xvf fx.tar.xz
-   rm fx.tar.xz
+   rm -f fx.tar.xz
    
-   echo start fivem service
+   echo "Starting FiveM service"
    systemctl start fivemserver.service
 
-   echo Waiting for next time to update fivem
-   sleep 48h
+   echo "Waiting for next update interval (${UPDATE_INTERVAL})"
+   sleep ${UPDATE_INTERVAL}
 done
