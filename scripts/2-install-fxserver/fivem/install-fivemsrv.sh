4495a8e28800bfd84c4525a28d26b0f9ca17a140
LizenzFass78851 <82592556+LizenzFass78851@users.noreply.github.com>
2025-03-16 13:36:38 +0000

    refactor(install-fivem): optimization

--- scripts/2-install-fxserver/fivem/install-fivemsrv.sh
+++ scripts/2-install-fxserver/fivem/install-fivemsrv.sh
@@ -10,36 +10,41 @@ DL_URL=${SRV_ADR}"$(wget -q -O - ${SRV_ADR} | grep -B 1 'LATEST RECOMMENDED' | t
 # for newer versions (experimental code to download the newer versions)
 # DL_URL=${SRV_ADR}"$(wget -q -O - ${SRV_ADR} | head -n 31 | tail -n 1 | cut -d '"' -f 4 | cut -c 2-)"
 
-
 # script
-mkdir -p ~/server/fivem
-cd ~/server/fivem
+INSTALL_DIR=~/server/fivem
+TXDATA_DIR=/txData
+SERVICE_FILE=/etc/systemd/system/fivemserver.service
 
-wget ${DL_URL}
+mkdir -p ${INSTALL_DIR}
+cd ${INSTALL_DIR}
 
-tar -xvf fx.tar.xz && \
-  rm fx.tar.xz
+wget ${DL_URL} -O fx.tar.xz
 
-mkdir /txData
-ln -s -r /txData/ ./txData
-chmod -R 777 /txData/
+tar -xvf fx.tar.xz && rm fx.tar.xz
 
-echo [Unit] >/etc/systemd/system/fivemserver.service
-echo # Abschnitt wird im Artikel systemd/Units beschrieben >>/etc/systemd/system/fivemserver.service
-echo Description=FiveM Server >>/etc/systemd/system/fivemserver.service
-echo  >>/etc/systemd/system/fivemserver.service
-echo [Service] >>/etc/systemd/system/fivemserver.service
-echo Type=simple >>/etc/systemd/system/fivemserver.service
-echo ExecStart=$(echo ~)/server/fivem/run.sh +set serverProfile dev_server +set txAdminPort 40125 >>/etc/systemd/system/fivemserver.service
-echo User=$(whoami) >>/etc/systemd/system/fivemserver.service
-echo Group=$(whoami) >>/etc/systemd/system/fivemserver.service
-echo WorkingDirectory=$(echo ~)/server/fivem >>/etc/systemd/system/fivemserver.service
-echo  >>/etc/systemd/system/fivemserver.service
-echo [Install] >>/etc/systemd/system/fivemserver.service
-echo # Abschnitt wird im Artikel systemd/Units beschrieben >>/etc/systemd/system/fivemserver.service
-echo WantedBy=multi-user.target >>/etc/systemd/system/fivemserver.service
+mkdir -p ${TXDATA_DIR}
+ln -s -r ${TXDATA_DIR} ./txData
+chmod -R 777 ${TXDATA_DIR}
+
+cat <<EOL > ${SERVICE_FILE}
+[Unit]
+# Abschnitt wird im Artikel systemd/Units beschrieben
+Description=FiveM Server
+
+[Service]
+Type=simple
+ExecStart=${INSTALL_DIR}/run.sh +set serverProfile dev_server +set txAdminPort 40125
+User=$(whoami)
+Group=$(whoami)
+WorkingDirectory=${INSTALL_DIR}
+
+[Install]
+# Abschnitt wird im Artikel systemd/Units beschrieben
+WantedBy=multi-user.target
+EOL
 
 systemctl enable fivemserver.service
 systemctl start fivemserver.service
-echo "systemctl status fivemserver.service" can be used to query the status of the service
-echo dont forget to use the command "chmod -R 777 /txData/" and repeat this if there is data or several users who should help cannot write to it
+
+echo "systemctl status fivemserver.service can be used to query the status of the service"
+echo "Don't forget to use the command 'chmod -R 777 /txData/' and repeat this if there is data or several users who should help cannot write to it"
