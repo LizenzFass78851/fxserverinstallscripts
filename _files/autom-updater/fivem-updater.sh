771fd6509def057943e5f54295c75800ff2ea39f
LizenzFass78851 <82592556+LizenzFass78851@users.noreply.github.com>
2025-03-16 13:46:36 +0000

    refactor(fivemupdater): more optimizations

--- _files/autom-updater/fivem-updater.sh
+++ _files/autom-updater/fivem-updater.sh
@@ -5,6 +5,7 @@ set -e # Exit the script on error
 SRV_ADR="https://runtime.fivem.net/artifacts/fivem/build_proot_linux/master/"
 SERVER_DIR=~/server/fivem
 UPDATE_INTERVAL=48h
+DOWNLOAD_FILE=fx.tar.xz
 
 while true
 do
@@ -19,31 +20,29 @@ do
    # for tagged versions
    #DL_URL=https://runtime.fivem.net/artifacts/fivem/build_proot_linux/master/9956-41b2e627e3b80ddbba4d63cb74968ac3d5926eb6/fx.tar.xz
 
-   if [ -f ./fx.tar.xz ]; then
+   if [ -f ./${DOWNLOAD_FILE} ]; then
         echo "Removing leftover files"
-        rm ./fx.tar.xz*
+        rm ./${DOWNLOAD_FILE}*
    fi
 
    echo "Downloading ${DL_URL}"
-   wget ${DL_URL}
-   if [ ! -f ./fx.tar.xz ]; then
+   wget ${DL_URL} -O "${DOWNLOAD_FILE}"
+   if [ ! -f ./${DOWNLOAD_FILE} ]; then
         echo "Waiting 5 seconds before retrying download"
         sleep 5s
         echo "Downloading ${DL_URL} again"
-        wget ${DL_URL}
-        if [ ! -f ./fx.tar.xz ]; then
+        wget ${DL_URL} -O "${DOWNLOAD_FILE}"
+        if [ ! -f ./${DOWNLOAD_FILE} ]; then
             echo "Failed to download ${DL_URL}, exiting"
             exit 1
         fi
    fi
    
    echo "Stopping FiveM service and removing old program files"
-   systemctl stop fivemserver.service
-   rm -rf alpine run.sh
+   systemctl stop fivemserver.service && rm -rf alpine run.sh
    
    echo "Extracting downloaded file"
-   tar -xvf fx.tar.xz
-   rm -f fx.tar.xz
+   tar -xvf "${DOWNLOAD_FILE}" && rm -f "${DOWNLOAD_FILE}"
    
    echo "Starting FiveM service"
    systemctl start fivemserver.service
