7242a6c6e243e9af0bcd8b227e50900f9f598e04
LizenzFass78851 <82592556+LizenzFass78851@users.noreply.github.com>
2025-03-16 13:27:37 +0000

    refactor(fastrestore-extrafiles): optimization

--- a_extra-files/fastrestore/FASTRESTORE-fivem.sh
+++ a_extra-files/fastrestore/FASTRESTORE-fivem.sh
@@ -1,10 +1,33 @@
 #!/bin/bash
 
-#echo restore fivem-db
-#cd ~/server/docker/phpmyadmin && docker-compose down && rm -r db && cp /root/server/backup/fivem-db-mount/root/server/docker/phpmyadmin/db/ -r . && chmod -R 777 db && docker-compose up -d && fusermount -u ~/server/backup/fivem-db-mount #run if -f was not used during mount
+set -e  # Exit immediately if a command exits with a non-zero status
+set -u  # Treat unset variables as an error and exit immediately
+set -o pipefail  # Prevents errors in a pipeline from being masked
 
-echo restore fivem
-cd ~/server/backup && systemctl stop fivemserver && rm -r /txData/ && cp fivem-mount/txData/ -r / && chmod -R 777 /txData/ && systemctl start fivemserver && fusermount -u ~/server/backup/fivem-mount #run if -f was not used during mount
+restore-db() {
+  echo "Restoring fivem-db"
+  cd ~/server/docker/phpmyadmin
+  docker-compose down
+  rm -rf db
+  cp -ra /root/server/backup/fivem-db-mount/root/server/docker/phpmyadmin/db/ .
+  chmod -R 777 db
+  docker-compose up -d
+  fusermount -u ~/server/backup/fivem-db-mount  # run if -f was not used during mount
+}
 
-echo script is done
+restore-fivem() {
+  echo "Restoring fivem"
+  cd ~/server/backup
+  systemctl stop fivemserver
+  rm -rf /txData/
+  cp -ra fivem-mount/txData/ /
+  chmod -R 777 /txData/
+  systemctl start fivemserver
+  fusermount -u ~/server/backup/fivem-mount  # run if -f was not used during mount
+}
+
+#restore-db
+restore-fivem
+
+echo "Script is done"
 
