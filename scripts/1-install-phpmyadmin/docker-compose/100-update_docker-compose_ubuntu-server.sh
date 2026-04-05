0e87b21632fb5445f684b6b6aeb16284fae31e67
LizenzFass78851 <82592556+LizenzFass78851@users.noreply.github.com>
2026-04-05 09:09:27 +0000

    refactor: improve update docker-compose script

--- scripts/1-install-phpmyadmin/docker-compose/100-update_docker-compose_ubuntu-server.sh
+++ scripts/1-install-phpmyadmin/docker-compose/100-update_docker-compose_ubuntu-server.sh
@@ -2,13 +2,17 @@
 
 set -e # Exit the script on error
 
-# script
-apt purge -y docker-compose && \
-  apt autoremove -y
-
-rm -f /usr/local/bin/docker-compose && \
-  rm -f /usr/bin/docker-compose
-
-echo -e '#!/bin/sh\nexec docker compose "$@"' | tee /usr/local/bin/docker-compose > /dev/null && \
-  ln -sf /usr/local/bin/docker-compose /usr/bin/docker-compose && \
-  chmod +x /usr/local/bin/docker-compose
+if docker compose version &> /dev/null; then
+    echo "Docker Compose always is installed."
+else
+    if [ -f /usr/local/bin/docker-compose ] && docker-compose version &> /dev/null; then
+        echo "Updating Docker Compose..."
+        rm -f /usr/local/bin/docker-compose && rm -f /usr/bin/docker-compose
+        curl -SL $(curl -L -s https://api.github.com/repos/docker/compose/releases/latest | grep -o -E "https://(.*)docker-compose-linux-$(uname -m)") -o /usr/local/bin/docker-compose
+        ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+        chmod +x /usr/local/bin/docker-compose
+    else
+        echo "Docker Compose is not installed. Exiting..."
+        exit 1
+    fi
+fi
